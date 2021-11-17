f93174d885452fb79cd90c42cd3f0265df8d8f83
b3rn4rd0m3y3r <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2021-11-17 18:41:00 -0300

    Add files via upload

---
+++ Site.js
@@ -0,0 +1,27 @@
+/*
+var MinhasSecoes = {
+	"Cabecalho" : [ { "Titulo0" : "Hotsite Projetos" } , { "Cor" : "royalblue" } ],
+	"Formato" : [
+			{ "Colunas": "4" }, 
+			{ "Serviços" : [
+				{ Titulo : "Titulo 01", Imagem : "Img01.jpg" },
+				{ Titulo : "Titulo 02", Imagem : "Img02.jpg" },
+				{ Titulo : "Titulo 03", Imagem : "Img03.jpg" }				
+				] }
+			]
+		};
+*/
+var MinhasSecoes = {
+	"Cabecalho" : [ { "Titulo0" : "Hotsite Projetos" } , { "Cor" : "royalblue" } ],
+	"Serviços" : {
+		"Formato" : [
+				{ "Colunas": "4" }
+				], 
+		"Itens" : [
+					{ Titulo : "Titulo 01", Imagem : "Img01.jpg" },
+					{ Titulo : "Titulo 02", Imagem : "Img02.jpg" },
+					{ Titulo : "Titulo 03", Imagem : "Img03.jpg" }				
+				]
+				}
+	};
+	
